7e135565963073ebddbdd1a8dc1088b9769dd730
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-12-09 18:27:18 -0300

    feat ecommerce and fix bg style footer

--- src/components/Ecommerce.js
+++ src/components/Ecommerce.js
@@ -1,5 +1,14 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
 const Ecommerce = () => {
-  return <div></div>;
+  return (
+    <>
+      <Box>
+        <Typography variant="h6">Ecommerce</Typography>
+      </Box>
+    </>
+  );
 };
 
 export default Ecommerce;
