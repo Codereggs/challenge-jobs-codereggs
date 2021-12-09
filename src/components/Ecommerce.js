8aeac36d668b693b293b5447b02b979f1c03247c
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-12-09 20:02:11 -0300

    ecommerce - mobile - pc grid

--- src/components/Ecommerce.js
+++ src/components/Ecommerce.js
@@ -1,12 +1,48 @@
-import { Typography } from "@mui/material";
+import { Collapse, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
 const Ecommerce = () => {
   return (
     <>
-      <Box>
-        <Typography variant="h6">Ecommerce</Typography>
-      </Box>
+      <Grid container className="mainShop">
+        <Grid
+          item
+          style={{ textAlign: "center", margin: "5rem" }}
+          xs={12}
+          md={12}
+        >
+          <Typography variant="p">Nuestras mejores bebidas</Typography>
+          <Typography variant="h4">Beer Shop</Typography>
+        </Grid>
+        {/* Filtro Mobile */}
+        <Collapse>
+          <Grid
+            item
+            style={{ textAlign: "center" }}
+            xs={12}
+            md={4}
+            sx={{ display: { xs: "none", md: "grid" } }}
+          >
+            <Typography variant="p">Nuestras mejores bebidas</Typography>
+            <Typography variant="h4">Beer Shop</Typography>
+          </Grid>
+        </Collapse>
+        {/* Filtro */}
+        <Grid
+          item
+          style={{ textAlign: "center" }}
+          md={4}
+          sx={{ display: { xs: "none", md: "grid" } }}
+        >
+          <Typography variant="p">Nuestras mejores bebidas</Typography>
+          <Typography variant="h4">Beer Shop</Typography>
+        </Grid>
+        {/* Cards */}
+        <Grid item style={{ textAlign: "center" }} xs={12} md={8}>
+          <Typography variant="p">Nuestras mejores bebidas</Typography>
+          <Typography variant="h4">Beer Shop</Typography>
+        </Grid>
+      </Grid>
     </>
   );
 };
