bded5990bab80ac2e5ae7ee237796c221b00da06
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-12-11 02:34:00 -0300

    fix mobile filter

--- src/components/filters/MobileFilter.js
+++ src/components/filters/MobileFilter.js
@@ -13,10 +13,24 @@ import {
 import { Box } from "@mui/system";
 import { useState } from "react";
 import { AiFillFilter } from "react-icons/ai";
+import {
+  getList,
+  orderByHigherABV,
+  orderByHigherIBU,
+  orderByHigherPH,
+  orderByLowABV,
+  orderByLowIBU,
+  orderByLowPH,
+} from "../../features/listGet";
+import { useDispatch } from "react-redux";
 
 const MobileFilter = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState([0, 1000]);
+  const [ABVSwitch, setABVSwitch] = useState([false, false]);
+  const [IBUSwitch, setIBUSwitch] = useState([false, false]);
+  const [PHSwitch, setPHSwitch] = useState([false, false]);
+  const dispatch = useDispatch();
 
   const handleChange = (event, newValue) => {
     let allPrices = document.querySelectorAll(".precio");
@@ -68,27 +82,117 @@ const MobileFilter = () => {
             <Typography variant="h6" style={{ textAlign: "start" }}>
               ABV:
             </Typography>
-            <FormControlLabel control={<Checkbox />} label="Mayor a Menor" />
-            <FormControlLabel control={<Checkbox />} label="Menor a Mayor" />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  onChange={(e) => {
+                    if (e.target.checked === true) {
+                      dispatch(orderByHigherABV());
+                      setABVSwitch([true, false]);
+                    } else {
+                      dispatch(getList());
+                      setABVSwitch([false, false]);
+                    }
+                  }}
+                  disabled={ABVSwitch[1]}
+                />
+              }
+              label="Mayor a Menor"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  onChange={(e) => {
+                    if (e.target.checked === true) {
+                      dispatch(orderByLowABV());
+                      setABVSwitch([false, true]);
+                    } else {
+                      dispatch(getList());
+                      setABVSwitch([false, false]);
+                    }
+                  }}
+                  disabled={ABVSwitch[0]}
+                />
+              }
+              label="Menor a Mayor"
+            />
 
             <hr />
             <Typography variant="h6" style={{ textAlign: "start" }}>
               IBU
             </Typography>
-            <FormControlLabel control={<Checkbox />} label="Mayor a Menor" />
-            <FormControlLabel control={<Checkbox />} label="Menor a Mayor" />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  onChange={(e) => {
+                    if (e.target.checked === true) {
+                      dispatch(orderByHigherIBU());
+                      setIBUSwitch([true, false]);
+                    } else {
+                      dispatch(getList());
+                      setIBUSwitch([false, false]);
+                    }
+                  }}
+                  disabled={IBUSwitch[1]}
+                />
+              }
+              label="Mayor a Menor"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  onChange={(e) => {
+                    if (e.target.checked === true) {
+                      dispatch(orderByLowIBU());
+                      setIBUSwitch([false, true]);
+                    } else {
+                      dispatch(getList());
+                      setIBUSwitch([false, false]);
+                    }
+                  }}
+                  disabled={IBUSwitch[0]}
+                />
+              }
+              label="Menor a Mayor"
+            />
             <hr />
             <Typography variant="h6" style={{ textAlign: "start" }}>
               PH
             </Typography>
-            <FormControlLabel control={<Checkbox />} label="Mayor a Menor" />
-            <FormControlLabel control={<Checkbox />} label="Menor a Mayor" />
-            <hr />
-            <Typography variant="h6" style={{ textAlign: "start" }}>
-              Volumen (Lts.)
-            </Typography>
-            <FormControlLabel control={<Checkbox />} label="Mayor a Menor" />
-            <FormControlLabel control={<Checkbox />} label="Menor a Mayor" />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  onChange={(e) => {
+                    if (e.target.checked === true) {
+                      dispatch(orderByHigherPH());
+                      setPHSwitch([true, false]);
+                    } else {
+                      dispatch(getList());
+                      setPHSwitch([false, false]);
+                    }
+                  }}
+                  disabled={PHSwitch[1]}
+                />
+              }
+              label="Mayor a Menor"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  onChange={(e) => {
+                    if (e.target.checked === true) {
+                      dispatch(orderByLowPH());
+                      setPHSwitch([false, true]);
+                    } else {
+                      dispatch(getList());
+                      setPHSwitch([false, false]);
+                    }
+                  }}
+                  disabled={PHSwitch[0]}
+                />
+              }
+              label="Menor a Mayor"
+            />
           </Box>
         </Collapse>
       </Grid>
